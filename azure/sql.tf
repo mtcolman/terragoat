aa2e7ffbfd32543875f3113eaee6c09d144d8f28
Matt Colman <MTCOLMAN@uk.ibm.com>
2022-10-14 09:50:28 +0100

    Update sql.tf

--- azure/sql.tf
+++ azure/sql.tf
@@ -13,6 +13,12 @@ resource "azurerm_sql_server" "example" {
   version                      = "12.0"
   administrator_login          = "ariel"
   administrator_login_password = "Aa12345678"
+  extended_auditing_policy {
+    storage_endpoint            = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key  = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+       retention_in_days        = 90
+  }
   tags = merge({
     environment = var.environment
     terragoat   = "true"
